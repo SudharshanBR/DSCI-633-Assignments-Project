9a78305624af173fa563b210e99156e169b887bd
Sudharshan B R <suddu@Sudharshans-MacBook-Air.local>
2021-01-27 17:37:21 -0500

    Comments were added

--- assignment1/A1.py
+++ assignment1/A1.py
@@ -1,16 +1,17 @@
-from sklearn.naive_bayes import GaussianNB
+# Importing DecisionTreeClassifier from sklearn.tree
+from sklearn.tree import DecisionTreeClassifier
 import pandas as pd
 
 if __name__ == "__main__":
     #  Load training data
     data_train = pd.read_csv("../data/Iris_train.csv")
     # Separate independent variables and dependent variables
-    independent = ["SepalLengthCm",	"SepalWidthCm",	"PetalLengthCm",	"PetalWidthCm"]
+    independent = ["SepalLengthCm", "SepalWidthCm", "PetalLengthCm", "PetalWidthCm"]
     X = data_train[independent]
     Y = data_train["Species"]
-    # Train model
-    clf = GaussianNB()
-    clf.fit(X,Y)
+    # Train model using DecisionTreeClassifier
+    clf = DecisionTreeClassifier()
+    clf.fit(X, Y)
     # Load testing data
     data_test = pd.read_csv("../data/Iris_test.csv")
     X_test = data_test[independent]
@@ -19,5 +20,5 @@ if __name__ == "__main__":
     # Predict probabilities
     probs = clf.predict_proba(X_test)
     # Print results
-    for i,pred in enumerate(predictions):
-        print("%s\t%f" %(pred,max(probs[i])))
+    for i, pred in enumerate(predictions):
+        print("%s\t%f" % (pred, max(probs[i])))
